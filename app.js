ad6c9839d0c2bd4409cbe87a5288dbdab3d36785
LauritzCode <lauritzmazin@gmail.com>
2024-10-06 23:27:33 +0200

    Added more logic, such as registering input-values of the grid-scale & event listeners functioning as placeholders for the upcoming logic for the painting function

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const colorMode = document.querySelector(".color-mode-btn");
 const eraseMode = document.querySelector(".erase-btn");
 const clearBtn = document.querySelector(".clear-btn");
 const sizeValue = document.querySelector(".size-value");
+const sizeSlider = document.querySelector("#sizeSlider");
+const colorPicker = document.querySelector(".color-picker");
 
-
-
+const createGrid = () => {
 const containerSize = 500;
-const divNumber= 256;
+const divNumber= sizeSlider.value * sizeSlider.value;
 const gridSize = Math.sqrt(divNumber);
 const divSize = containerSize / gridSize;
 
@@ -18,5 +19,33 @@ for (let i= 0; i < divNumber; i++){
   divs.style.width = `${divSize}px`;
   container.appendChild(divs);
 }
+}
 
-sizeValue.addEventListener();
+colorPicker.addEventListener("input", (e) => {
+})
+
+
+sizeSlider.addEventListener("input", (e) => {
+sizeValue.textContent = `${sizeSlider.value} x ${sizeSlider.value}`
+container.innerHTML = "";
+createGrid();
+}
+);
+
+document.querySelectorAll('.grid').forEach((div) => {
+    div.addEventListener("mousedown", () => {
+        isPainting = true;
+    });
+});
+
+document.querySelectorAll('.grid').forEach((div) => {
+    div.addEventListener("mousemove", () => {
+    if (isPainting) {
+    div.style.background = `${colorPicker.value}`
+    }
+    });
+});
+
+document.addEventListener("mouseup", () => {
+        isPainting = false;
+    });
